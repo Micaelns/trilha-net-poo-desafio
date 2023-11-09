530a44db2cee653ead2236975c80113f255e384c
Micael Souza <micael.ns@hotmail.com>
2023-11-09 14:38:46 -0300

    implementado testes de uso das classes

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia celNokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+Iphone celIphone = new Iphone("987654321", "Iphone 12", "987654321", 128);
+
+Console.WriteLine("##### Testes com o objeto Nokia: #####");
+celNokia.Ligar();
+celNokia.ReceberLigacao();
+celNokia.ReceberLigacao();
+celNokia.InstalarAplicativo("Facebook");
+celNokia.InstalarAplicativo("Instagram");
+
+Console.WriteLine();
+
+Console.WriteLine("##### Testes com o objeto Iphone: #####");
+celIphone.Ligar();
+celIphone.ReceberLigacao();
+celIphone.Ligar();
+celIphone.InstalarAplicativo("DIO");
+celIphone.InstalarAplicativo("Tinder");
+Console.WriteLine();
